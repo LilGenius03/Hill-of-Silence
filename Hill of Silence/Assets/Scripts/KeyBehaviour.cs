eeaa84f77e4c5e6ec81eea530536adaaa1117043
LilGenius03 <finnarcher03@gmail.com>
2024-01-15 19:11:19 +0000

    many changes made

--- Hill of Silence/Assets/Scripts/KeyBehaviour.cs
+++ Hill of Silence/Assets/Scripts/KeyBehaviour.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class KeyBehaviour : MonoBehaviour
 {
     [SerializeField] InventoryManager.AllItems _itemType;
-
+    public UnityEvent ItemCollected, ItemGone;
+    public float displayTime = 3f;
     private void OnTriggerEnter(Collider collision)
     {
         if(collision.CompareTag("Player"))
         {
             Debug.Log("Collide");
             InventoryManager.Instance.AddItem(_itemType);
-            Destroy(gameObject);
+            ItemCollected.Invoke();
+            StartCoroutine(DisplayTime());
         }
     }
 
-   
+    IEnumerator DisplayTime()
+    {
+        Debug.Log("Coroutine has started");
+        yield return new WaitForSecondsRealtime(displayTime);
+        ItemGone.Invoke();
+        Destroy(gameObject);
+        Debug.Log("Coroutine has ended");
+    }
 }
